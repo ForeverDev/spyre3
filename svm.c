612008a0cb162c02ed3a402ea64259c2970f762c
David Wilson <david.wilson@govsacademy.org>
2016-02-07 15:10:58 -0500

    update

--- svm.c
+++ svm.c
@@ -28,7 +28,7 @@ void spy_run(spy_state* S, const u8* code) {
 	// data is what we use to wrap 8 bytes
 	// into a double using memcpy
 	// memcpy(data, &code[S->reg[IP]], sizeof(f64));
-	f64 data;
+	u64 data;
 	while (1) {
 		u8 opcode = code[S->reg[IP]++];
 		switch (opcode) {
@@ -42,8 +42,8 @@ void spy_run(spy_state* S, const u8* code) {
 				switch (format) {
 					// mov reg0, imm 
 					case 0: 
-						memcpy(&data, &code[S->reg[IP]], sizeof(f64));
-						S->reg[IP] += sizeof(f64);	
+						memcpy(&data, &code[S->reg[IP]], sizeof(u64));
+						S->reg[IP] += sizeof(u64);	
 						S->reg[reg] = data;
 						break;
 					// mov reg0, reg1
@@ -51,14 +51,17 @@ void spy_run(spy_state* S, const u8* code) {
 						S->reg[reg] = S->reg[code[S->reg[IP]++]];
 						break;
 					// mov reg0, [reg1 + offset]
-					case 2: 
-						S->reg[reg] = S->mem[S->reg[S->reg[IP]++] + code[S->reg[IP]++]];
+					case 2: { 
+						u8 rightreg = S->reg[S->reg[IP]++];
+						S->reg[reg] = S->mem[rightreg + code[S->reg[IP]++]];
+						printf("%d\n", rightreg + code[S->reg[IP] - 1]);
 						break;
+					}
 					// mov [reg0 + offset], imm
 					case 3: {
-						u8 offset = code[S->reg[IP++]];
-						memcpy(&data, &code[S->reg[IP]], sizeof(f64));
-						S->reg[IP] += sizeof(f64);
+						u8 offset = code[S->reg[IP]++];
+						memcpy(&data, &code[S->reg[IP]], sizeof(u64));
+						S->reg[IP] += sizeof(u64);
 						S->mem[S->reg[reg] + offset] = data;
 					}
 					// mov [reg0 + offset], reg1
@@ -74,8 +77,18 @@ void spy_run(spy_state* S, const u8* code) {
 	}
 }
 
+void spy_toWords(spy_state* S, const u8* code) {
+	
+}
+
 void spy_debug(spy_state* S) {
 	for (u32 i = 0; i < 12; i++) {
-		printf("REG %02d: %d\n", i, S->reg[i]);
+		printf("REG %02d: %llu\n", i, S->reg[i]);
 	}	
+	printf("\n");
+	for (u32 i = 0; i < 65536; i++) {
+		if (S->mem[i] != 0) {
+			printf("MEM %08x: %llu\n", i, S->mem[i]);
+		}	
+	}
 }
--- svm.h
+++ svm.h
@@ -20,13 +20,14 @@ typedef double				f64;
 
 typedef struct spy_state {
 	
-	f64	mem[65536];
-	f64	reg[12];
+	u64	mem[65536];
+	u64	reg[12];
 
 } spy_state;
 
 spy_state*	spy_newstate();
 void		spy_run(spy_state*, const u8*);
+void		spy_toWords(spy_state*, const u8*);
 void		spy_debug(spy_state*);
 
 #endif
